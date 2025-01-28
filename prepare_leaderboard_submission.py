55f7401641ddbf2c3cc84b555fdf3b7ecbcca505
Rami Aly <rami-aly@hotmail.de>
2025-01-28 14:57:21 +0000

    update leaderboard file

--- prepare_leaderboard_submission.py
+++ prepare_leaderboard_submission.py
@@ -11,47 +11,18 @@ def convert(file_json, system_name):
 
     new_samples = []
     for i, sample in enumerate(samples):
-        if "gold" in file_json:
-            claim = sample['claim']
-            label = sample['label']
-            reference_evidence = ""
+        claim = sample['claim']
+        label = sample['pred_label']
+        prediction_evidence = ""
+        for src_qa in sample['evidence']:
+            prediction_evidence += src_qa["question"] + "\t\t\n" + src_qa["answer"] + "\t\t\n\n"
+        #
+        new_samples.append([i, claim, prediction_evidence, label, 'pred'])
 
-            if "questions" in sample:
-                for evidence in sample["questions"]:
-                    # If the answers is not a list, make them a list:
-                    if not isinstance(evidence["answers"], list):
-                        evidence["answers"] = [evidence["answers"]]
-
-                    for answer in evidence["answers"]:
-                        reference_evidence += evidence["question"] + "\t\t\n" + answer["answer"]
-                        #
-                        if "answer_type" in answer and answer[
-                            "answer_type"] == "Boolean" and "boolean_explanation" in answer:
-                            reference_evidence += ". " + answer["boolean_explanation"]
-
-                        reference_evidence += "\t\t\n\n"
-            new_samples.append([i, claim, reference_evidence, label, 'gold'])
-
-        if "pred" in file_json:
-            claim = sample['claim']
-            label = sample['pred_label']
-            prediction_evidence = ""
-            for src_qa in sample['evidence']:
-                prediction_evidence += src_qa["question"] + "\t\t\n" + src_qa["answer"] + "\t\t\n\n"
-            #
-            new_samples.append([i, claim, prediction_evidence, label, 'pred'])
-
-    if "gold" in file_json:
-        with open("solution1.csv", mode="w", newline="") as file:
-            writer = csv.writer(file)
-            writer.writerow(["id", "claim", "evi", "label", "split"])  # Write header
-            writer.writerows(new_samples)
-
-    if "pred" in file_json:
-        with open("leaderboard_submission/{}_submission.csv".format(system_name), mode="w", newline="") as file:
-            writer = csv.writer(file)
-            writer.writerow(["id", "claim", "evi", "label", "split"])  # Write header
-            writer.writerows(new_samples)
+    with open("leaderboard_submission/submission.csv".format(system_name), mode="w", newline="") as file:
+        writer = csv.writer(file)
+        writer.writerow(["id", "claim", "evi", "label", "split"])  # Write header
+        writer.writerows(new_samples)
 
     print("{} have been converted to .csv".format(file_json))
 
